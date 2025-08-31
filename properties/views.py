96ce2199dec795ffe4dd67b1716e1bf224536e63
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-08-31 22:11:13 +0000

    Caching in Django

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here. "@cache_page(60 * 15)"] properties/views.py doesn't contain: ["from django.views.decorators.cache import cache_page"] properties/views.py doesn't contain: ["from django.http import JsonResponse", "return JsonResponse({", "properties", "data"]
+# properties/views.py doesn't contain: ["def property_list(request):"]
